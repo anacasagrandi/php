bd41b9136ed949ad000420a72415fa250db714a9
anacasagrandi <ana.aninha.beatriz.06@gmail.com>
2023-08-10 14:21:35 -0300

    .

---
+++ Aula01/sintaxe1.php
@@ -0,0 +1,7 @@
+<?php 
+//geração de texto (string)
+ echo "Chama Sesi-Senai";
+// geração de texto estruturado (com tags, estruturado)
+echo "<h2> Gerando <span class='destaque'>HTML</span> através do PHP. </h2>";
+echo "<h2> Gerando <span class= \"destaque\">HTML</span> através do PHP. </h2>";
+?>
---
+++ Aula01/sintaxe3.php
@@ -0,0 +1,11 @@
+<?php 
+echo                                    "testando espaço em branco <br>";
+echo "testando espaço <br>"             ;
+// o caracter &nbsp; adiciona um espaço branco dentro da string 
+echo "&#160"; "&#160;               testando        espaço       em          branco <br>";
+"quebra de linha <br>"
+;
+
+echo "quebra de linha <br>"
+;
+?>
